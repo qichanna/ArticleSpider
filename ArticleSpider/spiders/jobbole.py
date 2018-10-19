a2950dabc9168a362aa2866b5f559d1502838220
liqi7 <liqi7@lenovo.com>
2018-10-19 19:31:26 +0800

    modify

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
+import re
 import scrapy
+import scrapy.http from Request
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/110287/']
 
     def parse(self, response):
-        re_selector = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()')
-        a = 0
-
+        re_selector = response.xpath('//*[@id="post-114405"]/div[1]/h1/text()')
+        print(re_selector.extract())
